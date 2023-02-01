c70cbeef573521703af903076d6e18c919cbb5bd
Aloke Chatterjee <84763630+alokec02@users.noreply.github.com>
2023-01-31 21:31:07 -0500

    Add files via upload

---
+++ DynamicDogs/DynamicDogs/Dog.cpp
@@ -0,0 +1,20 @@
+#include <string>
+using namespace std;
+#include "Dog.h"
+
+
+
+Dog::Dog(string name, string breed)
+{
+	this->name = name;
+	this->breed = breed;
+
+}
+string Dog::getName() const
+{
+	return name;
+}
+string Dog::getBreed() const
+{
+	return breed;
+}
