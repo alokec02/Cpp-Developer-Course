ccd0132958441c0571e1275684450d503f6de8e4
Aloke Chatterjee <84763630+alokec02@users.noreply.github.com>
2023-02-02 12:53:53 -0500

    Add files via upload

---
+++ DynamicFileRectangle/DynamicFileRectangle/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "rectangle.h"
+using namespace std;
+
+void populateVector(ifstream& infile, vector<Rectangle*>& rectVector);
+void printData(ofstream& outfile, vector<Rectangle*>& rectVector);
+void deallocateRectangles(vector<Rectangle*>& rectVector);
+
+
+int main()
+{
+	vector<Rectangle*> rectVector;
+	ifstream infile;
+	ofstream outfile;
+
+	infile.open("rectangles.txt");
+	outfile.open("output.txt");
+
+	int length;
+	int width;
+	
+
+	if (!infile)
+	{
+		cout << "Error opening file " << endl;
+		return 1;
+	}
+
+
+	populateVector(infile, rectVector);
+
+	printData(outfile, rectVector);
+	deallocateRectangles(rectVector);
+
+	
+	infile.close();
+	outfile.close();
+	return 0;
+}
+
+void populateVector(ifstream& infile, vector<Rectangle*>& rectVector)
+{
+	Rectangle* temp;
+	int length;
+	int width;
+
+	while (!infile.eof())
+	{
+		infile >> length;
+		infile >> width;
+
+		temp = new Rectangle(length, width);
+		rectVector.push_back(temp);
+
+	}
+}
+void printData(ofstream& outfile, vector<Rectangle*>& rectVector)
+{
+	for (Rectangle* rectPtr : rectVector)
+	{
+		outfile << rectPtr->area() << "\t" << rectPtr->perimeter() << endl;
+	}
+
+}
+void deallocateRectangles(vector<Rectangle*>& rectVector)
+{
+	for (Rectangle* rectPtr : rectVector)
+	{
+		delete rectPtr;
+	}
+
+	rectVector.clear();
+
+}
